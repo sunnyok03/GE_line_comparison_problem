ba235a5570c73602115f85b2455c812f81adba11
sunnyok03 <sunnyok03@gmail.com>
2023-11-23 23:14:10 +0530

    UC3 check for same length line

--- LineComparison.java
+++ LineComparison.java
@@ -1,24 +1,42 @@
 package com.line_comparison;
 
+import javax.sound.sampled.Line;
+import java.awt.*;
 import java.util.Scanner;
 
 public class LineComparison {
+
+    private static Point inputPoint(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter x and y: ");
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        return new Point(x,y);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Welcome to line comparison problem..");
 
         // finding distance between two points
-        System.out.print("Enter x1,y1 and x2,y2: ");
-        int x1 = sc.nextInt();
-        int y1 = sc.nextInt();
-        int x2 = sc.nextInt();
-        int y2 = sc.nextInt();
-
+        System.out.println("Enter two points:-");
+        Point p1 = inputPoint();
+        Point p2 = inputPoint();
         System.out.println("Distance between (x1,y1) and (x2,y2): "
-                + distanceBetweenTwoPoints(x1,y1,x2,y2));
+                + distanceBetweenTwoPoints(p1,p2));
 
+        // checking if two lines are equal
+        System.out.println("Enter two Lines each with two points:-");
+        Point p3 = inputPoint();
+        Point p4 = inputPoint();
+        Point p5 = inputPoint();
+        Point p6 = inputPoint();
 
+        if(isSameLine(p3,p4,p5,p6)){
+            System.out.println("Both lines length are equal.");
+        }else{
+            System.out.println("Both lines length are not equal.");
+        }
     }
 
     /*
@@ -26,9 +44,19 @@ public class LineComparison {
     @params: x1,y1,x2,y2
     @return: double
      */
-    public static double distanceBetweenTwoPoints(int x1,int y1,int x2,int y2){
-        int x = (x1-x2)*(x1-x2);
-        int y = (y1-y2)*(y1-y2);
+    public static double distanceBetweenTwoPoints(Point p1,Point p2){
+        int x = (p1.x-p2.x)*(p1.x-p2.x);
+        int y = (p1.y-p2.y)*(p1.y-p2.y);
         return Math.sqrt(x+y);
+    }
+    /*
+    Check if both lines length are same
+    @params:p1,p2,p3,p4 (end points of two lines)
+    @return: true if both lines length are same
+     */
+    public static boolean isSameLine(Point p1,Point p2,Point p3,Point p4){
+        double length1 = distanceBetweenTwoPoints(p1,p2);
+        double length2 = distanceBetweenTwoPoints(p3,p4);
+        return length2 == length1;
     }
 }
